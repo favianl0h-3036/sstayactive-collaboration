e180b727583c9bd976bc8b4f0d5d1fca8171c8a8
Favian <favian.lohdaiyong@gmail.com>
2020-08-26 16:37:45 +0800

    Initial Commit

---
+++ SSTayActive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSTayActive
+//
+//  Created by Favian LOH on 26/8/20.
+//  Copyright © 2020 Favian LOH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
